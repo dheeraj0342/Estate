e7f84475ae1263e699c88d0691220f3e5e6a054a
dheeraj singh <137195588+dheerajSgit@users.noreply.github.com>
2023-11-01 22:29:09 +0530

    create a signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,20 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Signin from './pages/Signin'
-import Signout from './pages/Signout'
+import Signup from './pages/Signup'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 
 const App = () => {
   return (
 <BrowserRouter>
+<Header></Header>
 <Routes>
   <Route  path="/" element={<Home/>} />
   <Route  path="/about" element={<About/>} />
   <Route  path="/sign-in" element={<Signin/>} />
-  <Route  path="/sign-out" element={<Signout/>} />
+  <Route  path="/sign-up" element={<Signup/>} />
   <Route  path="/profile" element={<Profile/>} />
 </Routes>
 </BrowserRouter>
