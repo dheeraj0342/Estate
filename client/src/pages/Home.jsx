ed252be87138fe3ae5fd7b48fd4ed303ef784849
dheeraj singh <137195588+dheerajSgit@users.noreply.github.com>
2023-11-01 23:13:10 +0530

    added loading functionality to signup button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Header from '..//components//Header.jsx'
 const Home = () => {
   return (
     <>
-     <Header></Header>
     </>
    
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -48,7 +48,7 @@ const Signup = () => {
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password'onChange={handleChange} />
-        <button  disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80' >signup</button>
+        <button  disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80' >{loading ? "loading.." : "Sign up"}</button>
       </form>
       <div className='flex gap-2 mt-5'>
         <p>
